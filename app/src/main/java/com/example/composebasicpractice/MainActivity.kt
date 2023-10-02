fa953faadad14547f7e7fac47021f585fb3d7a79
Jaehwa Noh <shwoghk14@gmail.com>
2023-10-02 15:58:32 +0900

    Separate Title and Text. - ArticleText - ArticleTitle

--- app/src/main/java/com/example/composebasicpractice/MainActivity.kt
+++ app/src/main/java/com/example/composebasicpractice/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -30,7 +32,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArticleText("Android")
+                    ArticleFormat(
+                        image = painterResource(id = R.drawable.bg_compose_background)
+                    )
                 }
             }
         }
@@ -38,21 +42,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ArticleFormat(image: Painter, modifier: Modifier = Modifier) {
+fun ArticleFormat(image: Painter) {
     Column {
-        Image(painter = image, contentDescription = null)
-        ArticleText(
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        ArticleTitle(
             text = "Jetpack Compose tutorial",
             fontSize = 24.sp,
             modifier = Modifier.padding(16.dp)
         )
-        ArticleText(text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.")
-        ArticleText(text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app's UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI's construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.")
+        ArticleText(
+            text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        ArticleText(
+            text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app's UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI's construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
 
 @Composable
-fun ArticleText(
+fun ArticleTitle(
     text: String,
     fontSize: TextUnit = TextUnit.Unspecified,
     modifier: Modifier
@@ -60,6 +74,18 @@ fun ArticleText(
     Text(
         text = text,
         fontSize = fontSize,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun ArticleText(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        textAlign = TextAlign.Justify,
         modifier = modifier
     )
 }
