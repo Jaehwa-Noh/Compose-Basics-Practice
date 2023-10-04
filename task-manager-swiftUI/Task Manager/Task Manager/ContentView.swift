10c22794219dfcf62ead1153986678a50829a1b7
shwoghk14 <shwoghk14@gmail.com>
2023-10-04 16:27:40 +0900

    Create Task Manager Project.

---
+++ task-manager-swiftUI/Task Manager/Task Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Task Manager
+//
+//  Created by Jaehwa Noh on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ task-manager-swiftUI/Task Manager/Task Manager/Task_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagerApp.swift
+//  Task Manager
+//
+//  Created by Jaehwa Noh on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
