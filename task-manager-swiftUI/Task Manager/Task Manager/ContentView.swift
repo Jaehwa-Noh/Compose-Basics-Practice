0dcf52ae0820e6295378c8cca083613ce8b7682c
shwoghk14 <shwoghk14@gmail.com>
2023-10-04 16:38:46 +0900

    Final work.
    - Add alignment at VStack
    - Add Padding

--- task-manager-swiftUI/Task Manager/Task Manager/ContentView.swift
+++ task-manager-swiftUI/Task Manager/Task Manager/ContentView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(name: "ic_task_completed")
+        VStack(alignment: .center) {
+            Image("ic_task_completed")
             Text("All tasks completed")
+                .fontWeight(.bold)
+                .padding(.top, 24)
+                .padding(.bottom, 8)
             Text("Nice work!")
+                .font(.system(size: 16))
         }
     }
 }
