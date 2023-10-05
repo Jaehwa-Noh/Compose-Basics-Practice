79ccd96804e31dd8dac4a2535f59047eed70f419
Jaehwa Noh <shwoghk14@gmail.com>
2023-10-05 15:30:10 +0900

    Add weight

--- compose-quadradrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ compose-quadradrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,23 +39,60 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun QuadrantContent() {
-    Column {
-        Row {
-            Card(Color(0xFFEADDFF))
-            Card(Color(0xFFD0BCFF))
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(modifier = Modifier.weight(1f)) {
+            Card(
+                Color(0xFFEADDFF),
+                title = "Text composable",
+                body = "Displays text and follows the recommended Material Design guidelines.",
+                modifier = Modifier.weight(1f)
+            )
+
+            Card(
+                Color(0xFFD0BCFF),
+                title = "Image composable",
+                body = "Creates a composable that lays out and draws a given Painter class object.",
+                modifier = Modifier.weight(1f)
+            )
         }
 
-        Row {
-            Card(Color(0xFFB69DF8))
-            Card(Color(0xFFF6EDFF))
+        Row(modifier = Modifier.weight(1f)) {
+            Card(
+                Color(0xFFB69DF8),
+                title = "Row composable",
+                body = "A layout composable that places its children in a horizontal sequence.",
+                modifier = Modifier.weight(1f)
+            )
+
+            Card(
+                Color(0xFFF6EDFF),
+                title = "Column composable",
+                body = "A layout composable that places its children in a vertical sequence.",
+                modifier = Modifier.weight(1f)
+            )
         }
     }
 }
 
 @Composable
-fun Card(color: Color, modifier: Modifier = Modifier) {
-    Surface(color = color) {
+fun Card(color: Color, title: String, body: String, modifier: Modifier = Modifier) {
+    Surface(
+        color = color,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                title
+            )
 
+            Text(
+                body
+            )
+        }
     }
 }
 
