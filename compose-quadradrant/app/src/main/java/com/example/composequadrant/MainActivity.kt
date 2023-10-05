2c80adf6aa42a51a59c8a626cbd0ad9734d7c771
Jaehwa Noh <shwoghk14@gmail.com>
2023-10-05 15:07:39 +0900

    Basic skeleton add.

--- compose-quadradrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ compose-quadradrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    QuadrantContent()
                 }
             }
         }
@@ -30,17 +32,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun QuadrantContent() {
+    Column {
+        Row {
+            Card(Color(0xFFEADDFF))
+            Card(Color(0xFFD0BCFF))
+        }
+
+        Row {
+            Card(Color(0xFFB69DF8))
+            Card(Color(0xFFF6EDFF))
+        }
+    }
+}
+
+@Composable
+fun Card(color: Color, modifier: Modifier = Modifier) {
+    Surface(color = color) {
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        QuadrantContent()
     }
 }
