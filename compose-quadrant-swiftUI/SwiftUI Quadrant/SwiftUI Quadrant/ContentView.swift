16d2259d75c4d34c0a17a6d35042a4ddf793bae5
shwoghk14 <shwoghk14@gmail.com>
2023-10-09 14:04:46 +0900

    Add minHieght, minWidth, maxHeight, maxWidth.

--- compose-quadrant-swiftUI/SwiftUI Quadrant/SwiftUI Quadrant/ContentView.swift
+++ compose-quadrant-swiftUI/SwiftUI Quadrant/SwiftUI Quadrant/ContentView.swift
@@ -11,12 +11,22 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HStack {
+                CardView(title: "Text composable", information: "Displays text and follows the recommended Material Design guidelines.")
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
                 
+                CardView(title: "Image composable", information: "Creates a composable that lays out and draws a given Painter class object.")
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
             }
+            .frame(minHeight: 0, maxHeight: .infinity)
+            
             
             HStack {
-                
+                CardView(title: "Row composable", information: "A layout composable that places its children in a horizontal sequence.")
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
+                CardView(title: "Column composable", information: "A layout composable that places its children in a vertical sequence.")
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
             }
+            .frame(minHeight: 0, maxHeight: .infinity)
         }
     }
 }
@@ -28,6 +38,8 @@ struct CardView: View {
     var body: some View {
         VStack {
             Text(title)
+                .fontWeight(.bold)
+                .padding(.bottom, 16)
             Text(information)
         }
         .padding(16)
